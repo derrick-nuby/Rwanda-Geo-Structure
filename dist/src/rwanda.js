da497da9c1ef5b3413a6f83ef83149e52ebd40f2
David MANZI <manzidavid093@gmail.com>
2025-12-09 18:58:01 +0200

    cache-enabled build files

--- dist/src/rwanda.js
+++ dist/src/rwanda.js
@@ -2,51 +2,101 @@
 import rwandaData from '../rwanda.json';
 // data import
 var data = rwandaData;
+var cache = {
+    provinces: null,
+    districts: null,
+    districtsByProvince: new Map(),
+    sectors: null,
+    sectorsByDistrict: new Map(),
+    cells: null,
+    cellsBySector: new Map(),
+    villages: null,
+    villagesByCell: new Map(),
+};
 // functions
 export var getCountry = function () {
     return 'Rwanda';
 };
 export var getProvinces = function () {
-    return Object.keys(data.rwanda);
+    if (cache.provinces)
+        return cache.provinces;
+    var res = Object.keys(data.rwanda);
+    cache.provinces = res;
+    return res;
 };
 export var getDistricts = function () {
-    return Object.values(data.rwanda).flatMap(function (province) { return Object.keys(province); });
+    if (cache.districts)
+        return cache.districts;
+    var res = Object.values(data.rwanda).flatMap(function (province) { return Object.keys(province); });
+    cache.districts = res;
+    return res;
 };
 export var getDistrictsByProvince = function (province) {
-    return Object.keys(data.rwanda[province] || {});
+    if (cache.districtsByProvince.has(province))
+        return cache.districtsByProvince.get(province);
+    var res = Object.keys(data.rwanda[province] || {});
+    cache.districtsByProvince.set(province, res);
+    return res;
 };
 export var getSectors = function () {
-    return Object.values(data.rwanda).flatMap(function (province) {
+    if (cache.sectors)
+        return cache.sectors;
+    var res = Object.values(data.rwanda).flatMap(function (province) {
         return Object.values(province).flatMap(function (district) { return Object.keys(district); });
     });
+    cache.sectors = res;
+    return res;
 };
 export var getSectorsByDistrict = function (province, district) {
     var _a;
-    return Object.keys(((_a = data.rwanda[province]) === null || _a === void 0 ? void 0 : _a[district]) || {});
+    var key = "".concat(province, "|").concat(district);
+    if (cache.sectorsByDistrict.has(key))
+        return cache.sectorsByDistrict.get(key);
+    var res = Object.keys(((_a = data.rwanda[province]) === null || _a === void 0 ? void 0 : _a[district]) || {});
+    cache.sectorsByDistrict.set(key, res);
+    return res;
 };
 export var getCells = function () {
-    return Object.values(data.rwanda).flatMap(function (province) {
+    if (cache.cells)
+        return cache.cells;
+    var res = Object.values(data.rwanda).flatMap(function (province) {
         return Object.values(province).flatMap(function (district) {
             return Object.values(district).flatMap(function (sector) { return Object.keys(sector); });
         });
     });
+    cache.cells = res;
+    return res;
 };
 export var getCellsBySector = function (province, district, sector) {
     var _a, _b;
-    return Object.keys(((_b = (_a = data.rwanda[province]) === null || _a === void 0 ? void 0 : _a[district]) === null || _b === void 0 ? void 0 : _b[sector]) || {});
+    var key = "".concat(province, "|").concat(district, "|").concat(sector);
+    if (cache.cellsBySector.has(key))
+        return cache.cellsBySector.get(key);
+    var res = Object.keys(((_b = (_a = data.rwanda[province]) === null || _a === void 0 ? void 0 : _a[district]) === null || _b === void 0 ? void 0 : _b[sector]) || {});
+    cache.cellsBySector.set(key, res);
+    return res;
 };
 export var getVillages = function () {
-    return Object.values(data.rwanda).flatMap(function (province) {
+    if (cache.villages)
+        return cache.villages;
+    var res = Object.values(data.rwanda).flatMap(function (province) {
         return Object.values(province).flatMap(function (district) {
             return Object.values(district).flatMap(function (sector) {
                 return Object.values(sector).flatMap(function (cell) { return cell; });
             });
         });
     });
+    cache.villages = res;
+    return res;
 };
 export var getVillagesByCell = function (province, district, sector, cell) {
     var _a, _b, _c;
-    return ((_c = (_b = (_a = data.rwanda[province]) === null || _a === void 0 ? void 0 : _a[district]) === null || _b === void 0 ? void 0 : _b[sector]) === null || _c === void 0 ? void 0 : _c[cell]) || [];
+    var key = "".concat(province, "|").concat(district, "|").concat(sector, "|").concat(cell);
+    if (cache.villagesByCell.has(key))
+        return cache.villagesByCell.get(key);
+    var res = ((_c = (_b = (_a = data.rwanda[province]) === null || _a === void 0 ? void 0 : _a[district]) === null || _b === void 0 ? void 0 : _b[sector]) === null || _c === void 0 ? void 0 : _c[cell]) || [];
+    cache.villagesByCell.set(key, res);
+    return res;
 };
 export var getRandomLocation = function () {
     var provinces = Object.keys(data.rwanda);
